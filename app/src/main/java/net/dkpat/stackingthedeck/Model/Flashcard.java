132a15563bf33e025e9a5eab5b8c683892b0f9b3
Dillion Patrick <dkpat12@gmail.com>
2015-12-06 14:23:53 -0500

    Flashcard demo content commented out

--- app/src/main/java/net/dkpat/stackingthedeck/Model/Flashcard.java
+++ app/src/main/java/net/dkpat/stackingthedeck/Model/Flashcard.java
@@ -15,51 +15,51 @@ import java.util.Map;
  * TODO: Replace all uses of this class before publishing your app.
  */
 public class Flashcard {
-
-    /**
-     * An array of sample (dummy) items.
-     */
+//
+//    /**
+//     * An array of sample (dummy) items.
+//     */
     public static final List<FlashcardItem> ITEMS = new ArrayList<FlashcardItem>();
-
-    /**
-     * A map of sample (dummy) items, by ID.
-     */
-    public static final Map<String, FlashcardItem> ITEM_MAP = new HashMap<String, FlashcardItem>();
-
-    private static final int COUNT = 25;
-
-    static {
-        // Add some sample items.
-        for (int i = 1; i <= COUNT; i++) {
-            addItem(createDummyItem(i));
-        }
-    }
-
-    private static void addItem(FlashcardItem item) {
-        ITEMS.add(item);
-        ITEM_MAP.put(item.id, item);
-    }
-
-    private static FlashcardItem createDummyItem(int position) {
-        return new FlashcardItem(String.valueOf(position), "Item " + position, makeDetails(position));
-    }
-
-    private static String makeDetails(int position) {
-        StringBuilder builder = new StringBuilder();
-        builder.append("Details about Item: ").append(position);
-        for (int i = 0; i < position; i++) {
-            builder.append("\nMore details information here.");
-        }
-        return builder.toString();
-    }
+//
+//    /**
+//     * A map of sample (dummy) items, by ID.
+//     */
+//    public static final Map<String, FlashcardItem> ITEM_MAP = new HashMap<String, FlashcardItem>();
+//
+//    private static final int COUNT = 25;
+//
+//    static {
+//        // Add some sample items.
+//        for (int i = 1; i <= COUNT; i++) {
+//            addItem(createDummyItem(i));
+//        }
+//    }
+//
+//    private static void addItem(FlashcardItem item) {
+//        ITEMS.add(item);
+//        ITEM_MAP.put(item.id, item);
+//    }
+//
+//    private static FlashcardItem createDummyItem(int position) {
+//        return new FlashcardItem(String.valueOf(position), "Item " + position, makeDetails(position));
+//    }
+//
+//    private static String makeDetails(int position) {
+//        StringBuilder builder = new StringBuilder();
+//        builder.append("Details about Item: ").append(position);
+//        for (int i = 0; i < position; i++) {
+//            builder.append("\nMore details information here.");
+//        }
+//        return builder.toString();
+//    }
 
     /**
      * A dummy item representing a piece of content.
      */
     public static class FlashcardItem {
-        public final String id;
-        public final String term;
-        public final String definition;
+        public  String id;
+        public  String term;
+        public  String definition;
 
         public FlashcardItem(String id, String term, String definition) {
             this.id = id;
@@ -70,6 +70,28 @@ public class Flashcard {
         @Override
         public String toString() {
             return term;
+        }
+
+        public String getflashcardId(){
+            return id;
+        }
+
+        public void setflashcardId (String flashcardId){
+            this.id = flashcardId;
+        }
+
+        public void setFlashcardTerm (String term){
+            this.term = term;
+        }
+
+        public String getFlshcardTerm(){
+            return term;
+        }
+
+        public String getDefintion() { return definition; }
+
+        public void setDefinition(String definition1){
+            this.definition = definition1;
         }
     }
 }
