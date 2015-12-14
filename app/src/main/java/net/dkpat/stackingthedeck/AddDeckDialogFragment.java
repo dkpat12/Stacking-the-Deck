1d4641007c5dd69e8577fec14872c350ad276923
Carissa Wong <cewong05@gmail.com>
2015-12-14 00:57:25 -0500

    xml files updated/created and ShareDeckDialogFragment created
    Edit/Create xml. Share deck xml. Updated New Deck Dialog xml. Framework
    of ShareDeckDialogFragment created. Still need functionality with Parse.

--- app/src/main/java/net/dkpat/stackingthedeck/AddDeckDialogFragment.java
+++ app/src/main/java/net/dkpat/stackingthedeck/AddDeckDialogFragment.java
@@ -1,45 +1,16 @@
 package net.dkpat.stackingthedeck;
 
-import android.app.Activity;
+import android.annotation.TargetApi;
 import android.app.AlertDialog;
 import android.app.Dialog;
-import android.app.DialogFragment;
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
-import android.content.Context;
 import android.content.DialogInterface;
-import android.content.Intent;
-import android.media.Image;
-import android.nfc.NfcAdapter;
+import android.os.Build;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.Window;
-import android.widget.Button;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.MenuInflater;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.AdapterView;
 import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.parse.ParseException;
 import com.parse.ParseUser;
-import com.parse.SaveCallback;
 
 import net.dkpat.stackingthedeck.Model.Deck;
-import net.dkpat.stackingthedeck.Model.Flashcard;
-import net.dkpat.stackingthedeck.helpers.DeckListAdapter;
-
-import java.util.Stack;
 
 /**
  * Created by cewon_000 on 12/8/2015.
@@ -67,11 +38,12 @@ public class AddDeckDialogFragment extends android.support.v4.app.DialogFragment
         super.onCreate(savedInstanceState);
     }
 
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         // Use the Builder class for convenient dialog construction
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setView(R.layout.add_deck_dialog);
+        builder.setView(R.layout.dialog_fragment_add_deck);
         builder.setMessage("Create a New Deck")
                 .setPositiveButton("Create", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
@@ -79,7 +51,7 @@ public class AddDeckDialogFragment extends android.support.v4.app.DialogFragment
 
                         Dialog f = (Dialog) dialog;
                         /* ERROR HERE! */
-                        EditText deckName = (EditText) f.findViewById(R.id.editText);
+                        EditText deckName = (EditText) f.findViewById(R.id.editTextAddDeck);
 
                         Deck deck = new Deck();
 
---
+++ app/src/main/java/net/dkpat/stackingthedeck/EditFlashcardActivity.java
@@ -0,0 +1,7 @@
+package net.dkpat.stackingthedeck;
+
+/**
+ * Created by C-Wo on 12/13/2015.
+ */
+public class EditFlashcardActivity {
+}
