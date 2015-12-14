b98402d92da5bd490df48743acd4463c90c4d8af
CEWong <c0wong01@louisville.edu>
2015-12-14 15:13:56 -0500

    updated edit flashcard xml file

--- app/src/main/java/net/dkpat/stackingthedeck/EditFlashcardActivity.java
+++ app/src/main/java/net/dkpat/stackingthedeck/EditFlashcardActivity.java
@@ -1,8 +1,10 @@
 package net.dkpat.stackingthedeck;
 
+import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -38,8 +40,9 @@ public class EditFlashcardActivity extends AppCompatActivity {
         setContentView(R.layout.activity_edit_flashcard);
 //
 //        String deckId = getIntent().getStringExtra("DeckId");
-
+        intent = getIntent();
         flashcard = new Flashcard();
+        flashcard.setObjectId(intent.getStringExtra("objectId"));
 
         editTextTerm = (TextView) findViewById(R.id.editTextTerm);
         editTextDefn = (TextView) findViewById(R.id.editTextDefn);
@@ -62,7 +65,44 @@ public class EditFlashcardActivity extends AppCompatActivity {
             }
         });
 
-//        btnSave = (Button) findViewById(R.)
+        btnSave = (Button) findViewById(R.id.btnSave);
+            btnSave.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    term = editTextTerm.getText().toString();
+                    defn = editTextDefn.getText().toString();
+
+                    if (term.equals("") && defn.equals("")) {
+                        Toast.makeText(getApplicationContext(),
+                                "Please enter name and description",
+                                Toast.LENGTH_LONG).show();
+
+                    } else {
+                        ParseQuery<ParseObject> query = ParseQuery.getQuery("Task");
+
+                        query.getInBackground(flashcard.getObjectId(), new GetCallback<ParseObject>() {
+                            public void done(ParseObject task, ParseException e) {
+                                if (e == null) {
+                                    task.put("term", term);
+                                    task.put("definition", defn);
+                                    task.saveInBackground();
+
+                                    Intent listActivity = new Intent(getApplicationContext(), ListActivity.class);
+                                    startActivity(listActivity);
+                                } else {
+                                    Toast.makeText(getApplicationContext(),
+                                            "Something went wrong",
+                                            Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        });
+
+
+                    }
+
+                }
+            });
 
 
     }
